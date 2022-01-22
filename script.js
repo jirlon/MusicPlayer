448281c0e447578ee2ceb3e46c53035d39d20890
Jirlon Oliveira <jirlon_jiw@hotmail.com>
2022-01-22 12:25:36 -0300

    add new song-function next, previous and updateBar

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let musicas = [
     {titulo:'A Year Ago', artista:'Neffex', src:'music/A Year Ago - NEFFEX.mp3', img:'image/guitar.jpg'},
     {titulo:'Believe', artista:'Neffex', src:'music/Believe - NEFFEX.mp3', img:'image/guitar2.jpg'},
-    {titulo:'Digifunk', artista:'DivKid', src:'music/Digifunk - DivKid.mp3', img:'image/eletronic.jpg'}
+    {titulo:'Digifunk', artista:'DivKid', src:'music/Digifunk - DivKid.mp3', img:'image/eletronic.jpg'},
+    {titulo:'Mess Call', artista:'USAF Heritage of America Band', src:'music/Mess Call - USAF Heritage of America Band.mp3', img:'image/note.jpg'}
 ];
 
 let musica = document.querySelector('audio');
@@ -16,32 +17,40 @@ let indexMusica = 0;
 
 renderizarMusica(indexMusica);
 
-duracaoMusica.textContent = segParaMin(Math.floor(musica.duration));
-
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
 
 document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
 
 musica.addEventListener('timeupdate', atualizarBarra);
 
+// retroceder musica
 document.querySelector('.anterior').addEventListener('click', () => { 
     indexMusica--;
     if(indexMusica < 0)
-        indexMusica = 2;
-    renderizarMusica(indexMusica)});
+        indexMusica = 3;
+    renderizarMusica(indexMusica)
+    pausarMusica();
+    tocarMusica();
+});
 
+// avançar musica 
 document.querySelector('.proxima').addEventListener('click', () => { 
     indexMusica++;
-    if(indexMusica >2)
+    if(indexMusica >3)
         indexMusica = 0;
-    renderizarMusica(indexMusica)});
+    renderizarMusica(indexMusica)
+    pausarMusica();
+    tocarMusica();
+});
 
+// tocar musica 
 function tocarMusica() {
     musica.play();
     document.querySelector('.botao-pause').style.display = 'block';
     document.querySelector('.botao-play').style.display = 'none';
 }
 
+// pausar musica
 function pausarMusica() {
     musica.pause();
     document.querySelector('.botao-pause').style.display = 'none';
@@ -53,6 +62,13 @@ function atualizarBarra() {
     barra.style.width = Math.floor((musica.currentTime/musica.duration) * 100) + '%';
     let tempoDecorrido = document.querySelector('.inicio');
     tempoDecorrido.textContent = segParaMin(Math.floor(musica.currentTime));
+    if(tempoDecorrido.textContent === duracaoMusica.textContent) {
+        if(indexMusica===musicas.length) indexMusica=0;
+        else indexMusica++;
+        renderizarMusica(indexMusica);
+        pausarMusica();
+        tocarMusica();
+    }
 }
 
 function segParaMin(segundos) {
